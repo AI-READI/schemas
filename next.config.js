6140d85c10da72cb8c2e3928d32cc43a74ba046e
Sanjay Soundarajan <sanjay.16@live.com>
2023-06-21 14:02:45 -0700

    feat: add schemas to the dataset

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["fairdataihub.org"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- scripts/pull-files.mjs
+++ scripts/pull-files.mjs
@@ -1,14 +1,23 @@
 import https from "https";
 import fs from "fs";
-import schemaListJSON from "../public/assets/data/fileList.json" assert { type: "json" };
+import schemaListJSON from "../data/fileList.json" assert { type: "json" };
+
+console.log("Delete the public/files folder...");
+
+if (!fs.existsSync("./public/files")) {
+  fs.mkdirSync("./public/files");
+} else {
+  fs.rmdirSync("./public/files", { recursive: true });
+  fs.mkdirSync("./public/files");
+}
 
 console.log("Pulling schemas to public folder...");
 
 schemaListJSON.forEach((schema) => {
   // Download the file to the public folder
-  const url = schema;
+  const url = schema.url;
   const filename = url.substring(url.lastIndexOf("/") + 1).split("?")[0];
-  const path = `./public/${filename}`;
+  const path = `./public/files/${filename}`;
   console.log(`Downloading ${filename} to ${path}`);
 
   const file = fs.createWriteStream(path);
